f9a1d657e2756760b77b43d369cedbabd2f57c28
Prathik Shetty <prathikshetty006@gmail.com>
2025-10-28 20:56:19 +0530

    Revert to original config - using Vercel only

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -3,8 +3,6 @@ import solid from "@astrojs/solid-js";
 import UnoCSS from "unocss/astro";
 
 export default defineConfig({
-  site: "https://prathik-26.github.io",
-  base: "/https://github.com/Prathik-26/my-portfolio",
   integrations: [
     UnoCSS({
       injectReset: true,
