69da2841fdf878bb4f8beb7e5feaee1f408602ef
Prathik Shetty <prathikshetty006@gmail.com>
2025-10-28 20:41:16 +0530

    Add GitHub Pages workflow

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config';
-import solid from '@astrojs/solid-js';
-import UnoCSS from 'unocss/astro';
+import { defineConfig } from "astro/config";
+import solid from "@astrojs/solid-js";
+import UnoCSS from "unocss/astro";
 
 export default defineConfig({
+  site: "https://prathik-26.github.io",
+  base: "/https://github.com/Prathik-26/my-portfolio",
   integrations: [
     UnoCSS({
-      injectReset: true 
+      injectReset: true,
     }),
     solid(),
   ],
